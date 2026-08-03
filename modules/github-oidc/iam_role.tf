cd03ab86752c3ae7186f460b99241992d3726296
muralidhargurram39 <muralidharaws39@gmail.com>
2026-08-03 07:24:10 +0000

    Test GitHub OIDC after trust policy fix

--- modules/github-oidc/iam_role.tf
+++ modules/github-oidc/iam_role.tf
@@ -30,12 +30,12 @@ data "aws_iam_policy_document" "github_assume_role" {
 
     condition {
 
-      test = "StringEquals"
+      test = "StringLike"
 
       variable = "token.actions.githubusercontent.com:sub"
 
       values = [
-        "repo:muralidhargurram39/aws-production-platform:*"
+        "repo:${var.github_owner}/${var.github_repository}:*"
       ]
     }
   }
