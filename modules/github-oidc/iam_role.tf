53cffe8fcc4b93ecd7faa71ac28067812a73addf
muralidhargurram39 <muralidharaws39@gmail.com>
2026-08-03 07:53:26 +0000

    Retest GitHub OIDC after trust policy update

--- modules/github-oidc/iam_role.tf
+++ modules/github-oidc/iam_role.tf
@@ -35,7 +35,7 @@ data "aws_iam_policy_document" "github_assume_role" {
       variable = "token.actions.githubusercontent.com:sub"
 
       values = [
-        "repo:${var.github_owner}/${var.github_repository}:*"
+        "repo:${var.github_owner}@*/${var.github_repository}@*:ref:refs/heads/*"
       ]
     }
   }
