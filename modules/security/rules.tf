588a485f15b94ee8e49910d514da260e52329d65
muralidhargurram39 <muralidharaws39@gmail.com>
2026-08-03 08:54:48 +0000

    Add descriptions to security group rules

--- modules/security/rules.tf
+++ modules/security/rules.tf
@@ -1,5 +1,6 @@
 resource "aws_vpc_security_group_ingress_rule" "alb_http" {
   security_group_id = aws_security_group.alb.id
+  description       = "Allow HTTP traffic from the Internet"
 
   ip_protocol = "tcp"
   from_port   = 80
@@ -9,6 +10,7 @@ resource "aws_vpc_security_group_ingress_rule" "alb_http" {
 }
 resource "aws_vpc_security_group_ingress_rule" "alb_https" {
   security_group_id = aws_security_group.alb.id
+  description       = "Allow HTTPS traffic from the Internet"
 
   ip_protocol = "tcp"
   from_port   = 443
@@ -18,13 +20,14 @@ resource "aws_vpc_security_group_ingress_rule" "alb_https" {
 }
 resource "aws_vpc_security_group_egress_rule" "alb_all" {
   security_group_id = aws_security_group.alb.id
-
-  ip_protocol = "-1"
+  description       = "Allow outbound traffic from the ALB"
+  ip_protocol       = "-1"
 
   cidr_ipv4 = "0.0.0.0/0"
 }
 resource "aws_vpc_security_group_ingress_rule" "application_http" {
   security_group_id = aws_security_group.application.id
+  description       = "Allow HTTP traffic from the ALB"
 
   ip_protocol = "tcp"
   from_port   = 80
@@ -35,12 +38,16 @@ resource "aws_vpc_security_group_ingress_rule" "application_http" {
 resource "aws_vpc_security_group_egress_rule" "application_all" {
   security_group_id = aws_security_group.application.id
 
+  description = "Allow outbound traffic from application instances"
+
   ip_protocol = "-1"
 
   cidr_ipv4 = "0.0.0.0/0"
 }
 resource "aws_vpc_security_group_ingress_rule" "database_postgres" {
   security_group_id = aws_security_group.database.id
+
+  description = "Allow PostgreSQL access from application instances"
 
   ip_protocol = "tcp"
   from_port   = 5432
@@ -51,12 +58,17 @@ resource "aws_vpc_security_group_ingress_rule" "database_postgres" {
 resource "aws_vpc_security_group_egress_rule" "database_all" {
   security_group_id = aws_security_group.database.id
 
+  description = "Allow outbound traffic from database instance"
+
   ip_protocol = "-1"
 
   cidr_ipv4 = "0.0.0.0/0"
 }
 resource "aws_vpc_security_group_egress_rule" "management_all" {
   security_group_id = aws_security_group.management.id
+
+  description = "Allow outbound traffic from management resources"
+
 
   ip_protocol = "-1"
 
